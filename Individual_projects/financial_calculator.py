f1e84eb435e2e4719288a97a923e806b6e0d3c36
TheCoolerRyan <ryan.crop@ucas-edu.net>
2026-01-08 19:17:06 +0000

    Made a bunch of functions

--- Individual_projects/financial_calculator.py
+++ Individual_projects/financial_calculator.py
@@ -7,22 +7,16 @@ def sales():
         #Ask Them the amount it costs, <-
         cost = input("Tell me the original cost of the item (Before the sale):").strip()
         #Stupid proff
-        number = cost.isdigit()
-        if number == True and int(cost) >= 0:
-            cost = int(cost)
+        test, cost = proff(cost)
+        if test == True:
             break
-        else:
-            print("That is not a valid number for the cost.")
     #Then do the same for the percent that is taken of
     while True:
         sale = input("Tell me the discount (Just the number! No signs,just the number, for example if its 15% of, put 15):").strip()
         #stupid proff
-        number = sale.isdigit()
-        if number == True and int(sale) >= 0:
-            sale = int(sale)
+        test,sale = proff(sale)
+        if test == True:
             break
-        else:
-            print("That is not a valid number for the sale.")
     #Create a calculation inner function
     def calculation():
         percent = sale/100
@@ -39,20 +33,16 @@ def goal():
     while True:
         goal = input("How much money do you want to save up to:").strip()
         #Stupid proff it
-        if goal.isdigit() == True and int(goal) > 0:
-            goal = int(goal)
+        test,goal = proff(goal)
+        if test == True:
             break
-        else:
-            print("That is not a valid option")
     #Figure out how much there putting in weekly
     while True:
         deposit = input("How much money do you deposit weekly:").strip()
         #Stupid proff it
-        if deposit.isdigit() == True and int(deposit) > 0:
-            deposit = int(deposit)
+        test,deposit = proff(deposit)
+        if test == True:
             break
-        else:
-            print("That is not a valid option")
     #Calculate amount of weeks
     weeks = goal/deposit
     #Tell them how many weeks it will take
@@ -63,37 +53,43 @@ def compound():
     while True:
         bank = input("How much money do you want to put into the bank:").strip()
         #Stupid proff it
-        if bank.isdigit() == True and int(bank) > 0:
-            bank = int(bank)
+        test,bank = proff(bank)
+        if test == True:
             break
-        else:
-            print("That is not a valid option")
     #Ask how long they are going to wait
     while True:
-        time = input("How long are you going to wait:").strip()
+        time = input("How many months are you going to wait:").strip()
         #Stupid proff it
-        if time.isdigit() == True and int(time) > 0:
-            time = int(time)
+        test,time = proff(time)
+        if test == True:
+            num = time
+            
             break
-        else:
-            print("That is not a valid option")
     #Ask how much they are trying to save up to
     while True:
-        rate = input("What is the interest rates?:").strip()
+        rate = input("What is the interest rates? (if its 1%, put 1):").strip()
         #Stupid proff it
-        if rate.isdigit() == True and int(rate) > 0:
-            rate = int(rate)
+        test,rate = proff(rate)
+        if test == True:
+            percent = rate/100
             break
-        else:
-            print("That is not a valid option")
+    #create for loop that allows for the loop to calculate
+    for i in range(num+1):
+        bank += bank*rate
+    print(f"After {time} months, you have ${bank}!")
+#create stupid proff function
 def proff(variable):
     #Ask how much they are trying to save up to
-    while True:
-        variable = input("How much money do you want to save up to:").strip()
         #Stupid proff it
         if variable.isdigit() == True and int(variable) > 0:
             variable = int(variable)
-            break
+            return True, variable
         else:
             print("That is not a valid option")
+            return False, variable
 #Use proof to simplfy code
+
+
+
+#Fix compound, its math is wrong
+compound()
