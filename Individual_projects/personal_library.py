00dd3b4bcc8a696007da6db690805a0b1a82ae58
TheCoolerRyan <ryan.crop@ucas-edu.net>
2026-01-15 09:52:48 -0700

    Made a bunch of pseudocode

--- Individual_projects/personal_library.py
+++ Individual_projects/personal_library.py
@@ -1,1 +1,32 @@
 #RC 1st, personal library program
+
+#Create list for lists of books and authors
+#Create tupple of the functions
+
+#Create add function
+    #Ask for them to input the title
+    #Ask them to input the author
+    #Then put those to values into a list and add that to the list
+    #Print off what you have added
+
+#Create view function
+    #For everything in the list, print of book name by author
+
+#Create remove function
+    #for every list in the list, print off them numbered and author by name
+
+    #Have them input what number they would like to get rid of
+    #Then remove bassed of the index.
+
+    #Print off what they have removed
+
+#Create search function
+    #Ask them if they would like to search based of off title or author
+    #if its based of off author then for each list in the list, check the list[1] for the author
+    #if its based of off title, then for each list in the list, check the list[0] for the title
+
+
+#Create main function
+        #Ask them what function they would like while in a while loop
+        #Check if its in a tupple
+    #if
