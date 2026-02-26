7663020e783daa354b44f2332bdef2429ae17e05
TheCoolerRyan <ryan.crop@ucas-edu.net>
2026-02-26 09:52:52 -0700

    Started on the fractal pattern generator

--- Individual_projects/fractal_pattern.py
+++ Individual_projects/fractal_pattern.py
@@ -1,30 +1,11 @@
 #RC 1st, Fractal pattern generator
 
 #Import turtle
-
+import turtle
 
 
 #Create dictionary that will create the fractal
-
-
-
-
-#Create dictionary that will change colors
-
-
-
-
-#Create main
-
-
-
-
-
-#Example to work of off
-import turtle
-
 def draw_triangle(points, color, my_turtle):
-    """Draws a single filled triangle."""
     my_turtle.fillcolor(color)
     my_turtle.up()
     my_turtle.goto(points[0][0], points[0][1])
@@ -35,6 +16,41 @@ def draw_triangle(points, color, my_turtle):
     my_turtle.goto(points[0][0], points[0][1])
     my_turtle.end_fill()
 
+
+
+#Create function that does the details of the sierpinski
+
+
+
+
+#Create main
+def main():
+    #Figure out the amount of depth that they want
+    #Figure out the color
+    my_points = [[-200, -150], [0, 200], [200, -150]]
+
+    #Ill call the main function to create the complex triangle and inside of it will be the draw triangle function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Example to work of off
+import turtle
 def get_mid(p1, p2):
     """Calculates the midpoint between two coordinates."""
     return ((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2)
@@ -61,30 +77,3 @@ def sierpinski(points, degree, my_turtle, user_color):
                     get_mid(points[0], points[2])],
                    degree-1, my_turtle, user_color)
 
-def main():
-    # --- User Input Section ---
-    print("--- Sierpinski Triangle Generator ---")
-    try:
-        user_depth = int(input("Enter recursion depth (e.g., 3, 4, or 5): "))
-        user_color = input("Enter a color name (e.g., 'blue', 'purple', 'forestgreen'): ").lower()
-    except ValueError:
-        print("Invalid depth. Defaulting to 3.")
-        user_depth = 3
-    
-    # Setup Turtle
-    my_turtle = turtle.Turtle()
-    my_screen = turtle.Screen()
-    my_turtle.speed(0)  # 0 is the fastest speed
-    
-    # Define initial triangle points [x, y]
-    # These coordinates center the triangle on the screen
-    my_points = [[-200, -150], [0, 200], [200, -150]]
-    
-    # Execute
-    sierpinski(my_points, user_depth, my_turtle, user_color)
-    
-    print("Drawing complete! Click the window to exit.")
-    my_screen.exitonclick()
-
-
-main()
